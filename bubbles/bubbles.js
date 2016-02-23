728fabc540fc773e08fe55609eca818f89fb4e9a
WiseClock <kuanghao04@gmail.com>
2016-02-22 20:02:09 -0800

    initial commit.

---
+++ bubbles/bubbles.js
@@ -0,0 +1,161 @@
+bubbleJson = [
+   {
+      "image": "bubbles/001.png",
+      "imageSelf": "bubbles/000.png",
+      "color": "#252424",
+      "bgcolor": "#cdd7e2",
+      "bgcolorSelf": "#64b9e4",
+      "border":
+      {
+         "top": 14,
+         "right": 8,
+         "bottom": 7,
+         "left": 14
+      },
+      "margin":
+      {
+         "top": 13,
+         "bottom": 10,
+         "right": 10
+      }
+   },
+   {
+      "image": "bubbles/002.png",
+      "color": "#713a04",
+      "bgcolor": "#efd2b9",
+      "border":
+      {
+         "top": 11,
+         "right": 17,
+         "bottom": 18,
+         "left": 10
+      },
+      "margin":
+      {
+         "top": 13,
+         "bottom": 11
+      }
+   },
+   {
+      "image": "bubbles/003.png",
+      "color": "#ffffff",
+      "bgcolor": "#ff9e6f",
+      "border":
+      {
+         "top": 15,
+         "right": 18,
+         "bottom": 14,
+         "left": 14
+      },
+      "margin":
+      {
+         "top": 13,
+         "bottom": 7
+      }
+   },
+   {
+      "image": "bubbles/004.png",
+      "color": "#834105",
+      "bgcolor": "#ffc666",
+      "border":
+      {
+         "top": 15,
+         "right": 15,
+         "bottom": 14,
+         "left": 11
+      },
+      "margin":
+      {
+         "top": 13,
+         "bottom": 6
+      }
+   },
+   {
+      "image": "bubbles/005.png",
+      "color": "#ffffff",
+      "bgcolor": "#4a115d",
+      "border":
+      {
+         "top": 12,
+         "right": 17,
+         "bottom": 17,
+         "left": 16
+      },
+      "margin":
+      {
+         "top": 13,
+         "bottom": 15
+      }
+   },
+   {
+      "image": "bubbles/006.png",
+      "color": "#663c12",
+      "bgcolor": "#fff1c0",
+      "border":
+      {
+         "top": 12,
+         "right": 22,
+         "bottom": 12,
+         "left": 14
+      },
+      "margin":
+      {
+         "top": 14,
+         "bottom": 12,
+         "right": 19
+      }
+   },
+   {
+      "image": "bubbles/007.png",
+      "color": "#ffe69b",
+      "bgcolor": "#d82740",
+      "border":
+      {
+         "top": 6,
+         "right": 14,
+         "bottom": 17,
+         "left": 12
+      },
+      "margin":
+      {
+         "top": 9,
+         "bottom": 9,
+         "right": 17
+      }
+   },
+   {
+      "image": "bubbles/008.png",
+      "color": "#585858",
+      "bgcolor": "#ffffff",
+      "border":
+      {
+         "top": 12,
+         "right": 9,
+         "bottom": 16,
+         "left": 17
+      },
+      "margin":
+      {
+         "top": 13,
+         "bottom": 13,
+         "right": 14
+      }
+   },
+   {
+      "image": "bubbles/009.png",
+      "color": "#16476b",
+      "bgcolor": "#acd9f8",
+      "border":
+      {
+         "top": 22,
+         "right": 23,
+         "bottom": 8,
+         "left": 13
+      },
+      "margin":
+      {
+         "top": 16,
+         "bottom": 10
+      }
+   }
+]
